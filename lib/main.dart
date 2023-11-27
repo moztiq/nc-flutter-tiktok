8da7cff412d82b736c02274fdc51240622bd3647
Seongki Kim <moztiq@gmail.com>
2023-11-27 12:25:18 +0900

    constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nc_flutter_tiktok/constants/gaps.dart';
+
+import 'constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -15,7 +18,18 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: const Row(
+            children: [
+              Text('hello'),
+              Gaps.h20,
+              Text('hello'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
