dbb0119e07e8b97465137785589db2ea9d7b1f9e
Seongki Kim <moztiq@gmail.com>
2023-12-08 15:09:01 +0900

    GridItem

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nc_flutter_tiktok/constants/gaps.dart';
 import 'package:nc_flutter_tiktok/constants/sizes.dart';
 
 final tabs = ['Top', 'Users', 'Videos', 'Sounds', 'LIVE', 'Shopping', 'Brands'];
@@ -45,13 +47,65 @@ class DiscoverScreen extends StatelessWidget {
                     crossAxisCount: 2,
                     crossAxisSpacing: Sizes.size10,
                     mainAxisSpacing: Sizes.size10,
-                    childAspectRatio: 9 / 16,
+                    childAspectRatio: 9 / 20,
                   ),
-                  itemBuilder: (context, index) => Container(
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text('$index'),
-                    ),
+                  itemBuilder: (context, index) => Column(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                          placeholder: "assets/images/placeholder.jpg",
+                          image:
+                              "https://images.unsplash.com/photo-1545038495-06c466616626?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmlnJTIwc2l6ZXxlbnwwfHwwfHx8MA%3D%3D",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Gaps.v10,
+                      const Text(
+                        'The image is forced to maintain the specified aspect ratio, regardless of the original proportions of the image. This means that the original proportions may be distorted as the image is scaled to fit the specified ratio.',
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Gaps.v5,
+                      DefaultTextStyle(
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        child: Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: Sizes.size12,
+                              backgroundImage: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/12403584?v=4',
+                              ),
+                            ),
+                            Gaps.h4,
+                            const Expanded(
+                              child: Text(
+                                'My avatar is going to be very long',
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Gaps.h4,
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size16,
+                              color: Colors.grey.shade600,
+                            ),
+                            Gaps.h2,
+                            const Text(
+                              '2.5M',
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
