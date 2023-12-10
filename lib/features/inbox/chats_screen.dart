7fc8858fbe7e53cbd487af1d44db55cbfff6b83a
Seongki Kim <moztiq@gmail.com>
2023-12-10 15:40:03 +0900

    AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nc_flutter_tiktok/constants/sizes.dart';
+import 'package:nc_flutter_tiktok/features/inbox/chat_detail_screen.dart';
+import 'package:nc_flutter_tiktok/features/main_navigation/main_navigation_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -10,6 +12,80 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final Duration _duration = Duration(
+    milliseconds: 300,
+  );
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: _duration,
+      );
+      _items.add(_items.length);
+    }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: _makeTile(index),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: Sizes.size24,
+        backgroundImage: NetworkImage(
+          'https://image.cine21.com/resize/cine21/still/2017/1218/10_39_22__5a371c4aaae6b[X252,310].jpg',
+        ),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            '$index',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            '2:16 PM',
+            style: TextStyle(
+              color: Colors.grey.shade400,
+              fontSize: Sizes.size12,
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
+      ),
+      subtitle: const Text('Good night. See you tomorrow!'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,48 +94,27 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text('Direct Messages'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
           )
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: Sizes.size24,
-              backgroundImage: NetworkImage(
-                'https://image.cine21.com/resize/cine21/still/2017/1218/10_39_22__5a371c4aaae6b[X252,310].jpg',
-              ),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  'Title',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  '2:16 PM',
-                  style: TextStyle(
-                    color: Colors.grey.shade400,
-                    fontSize: Sizes.size12,
-                    fontWeight: FontWeight.w600,
-                  ),
-                )
-              ],
-            ),
-            subtitle: const Text('Good night. See you tomorrow!'),
-          )
-        ],
+        // initialItemCount: 2,
+        itemBuilder: (context, index, animation) => FadeTransition(
+          key: UniqueKey(),
+          opacity: animation,
+          child: SizeTransition(
+            sizeFactor: animation,
+            child: _makeTile(index),
+          ),
+        ),
       ),
     );
   }
