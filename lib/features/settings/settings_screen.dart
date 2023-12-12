bec857dce95a318f55fd184adb7a99a848d37913
Seongki Kim <moztiq@gmail.com>
2023-12-12 10:47:50 +0900

    showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -14,15 +14,38 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () => showAboutDialog(
+            onTap: () async {
+              final date = await showDatePicker(
                 context: context,
-                applicationVersion: '1.0',
-                applicationLegalese: 'All rights reserved.'),
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now(),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+              final booking = await showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime(2001),
+                  lastDate: DateTime(2002),
+                  builder: (context, child) {
+                    return Theme(
+                      data: ThemeData(
+                        appBarTheme: AppBarTheme(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                      child: child!,
+                    );
+                  });
+              print(booking);
+            },
             title: Text(
-              'About',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
+              'What is your birthday?',
             ),
             subtitle: Text('About this app...'),
           ),
